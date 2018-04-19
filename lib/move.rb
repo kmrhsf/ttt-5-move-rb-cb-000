e67d775fee50cd4799b5073994fcb4e414d1a9ae
Kevin Mikami <mikamikm@gmail.com>
2018-04-19 20:15:18 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -13,8 +13,17 @@ end
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def move(array, index, value = "X")
-  array[index] = value
-  index = converted_input
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  board = Array.new(9, " ")
+  move(board, 0, "X")
+  move(board, 1, "O")
+  move(board, 2, "X")
+  move(board, 3, "O")
+  move(board, 4, "X")
+  move(board, 5, "O")
+  move(board, 6, "X")
+  move(board, 7, "X")
+  move(board, 8, "O") 
 
 end
 
