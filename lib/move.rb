ca70a897bb8ddac31db404c008ddbdaf98d475bc
Kevin Mikami <mikamikm@gmail.com>
2018-04-19 19:40:44 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -5,3 +5,11 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def input_to_index(user_input)
+
+  converted_input = user_input.to_i
+end
+
+
+# codenput_to_index and move method here!
